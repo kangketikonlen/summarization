1e0b41c27202a2478ddec6e6574936e3f01ba760
Gilang Pratama <pratamapriadi96@gmail.com>
2021-12-15 19:29:41 +0700

    :zap: Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ def read_root():
     return {"success": True, "results": "Ehe"}
 
 
-@app.post("/sumv1/")
+@app.post("/sumv1")
 def create_item(artikel: Artikel):
     summary = sum_v1(artikel.deskripsi)
     return {"success": True, "results": summary}
 
 
-@app.post("/sumv2/")
+@app.post("/sumv2")
 def create_item(url: ArtkelV2):
     summary = sum_v2(url.url)
     return {"success": True, "results": summary}
 
 
-@app.post("/sumv3/")
+@app.post("/sumv3")
 def create_item(artikel: Artikel):
     summary = sum_v3(artikel.deskripsi)
     return {"success": True, "results": summary}
