881b65dfa4527b8ab784d2e8f1c5b85403c8166b
Gilang Pratama <pratamapriadi96@gmail.com>
2021-12-16 05:37:27 +0700

    :zap: Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 HF_DATASETS_OFFLINE=1
 TRANSFORMERS_OFFLINE=1
 
-uvicorn main:app --reload --host 0.0.0.0 --port 2022 --workers 100
+# uvicorn main:app --reload --host 0.0.0.0 --port 2022
+gunicorn main:app --bind=0.0.0.0:2022 -k uvicorn.workers.UvicornWorker --timeout 1000
