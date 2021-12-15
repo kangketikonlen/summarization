7d5b5e755fcc74f4b4bffad43ebf56a3ec9c720f
Gilang Pratama <pratamapriadi96@gmail.com>
2021-12-15 17:31:43 +0700

    :zap: Update summarization

--- utils/summarization.py
+++ utils/summarization.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 
 def sum_v1(artikel):
-    summarizer = pipeline("summarization")
+    summarizer = pipeline(model="sshleifer/distilbart-cnn-12-6")
     summary = summarizer(
         artikel, max_length=200, min_length=20, num_workers=1000, batch_size=1
     )
