87d69d7b37946d736e80d31bf7d2aad86af729c5
Gilang Pratama <pratamapriadi96@gmail.com>
2021-12-15 19:18:48 +0700

    :zap: Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ HF_DATASETS_OFFLINE=1
 TRANSFORMERS_OFFLINE=1
 CUDA_LAUNCH_BLOCKING=1
 
-uvicorn main:app --reload --host 0.0.0.0 --port 2022
+uvicorn main:app --reload --host 0.0.0.0 --port 2022 --workers 100
